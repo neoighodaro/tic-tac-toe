e69998e74f30e1d9f2f18f54a486e64abd7d74bf
Neo Ighodaro <neo@creativitykills.co>
2018-06-11 11:18:48 +0100

    Improved code

--- app/Game/BoardState.php
+++ app/Game/BoardState.php
@@ -28,15 +28,30 @@ class BoardState implements Arrayable, Jsonable
     ];
 
     /**
+     * An array of moves made.
+     *
+     * @var \Illuminate\Support\Collection
+     */
+    protected $moves;
+
+    /**
      * Class constructor
      *
      * @param array $state
      */
     public function __construct()
     {
+        $this->moves = new Collection([]);
+
         $this->state = new Collection($this->defaultState);
     }
 
+    /**
+     * Adds a new tile to the board state.
+     *
+     * @param Tile|array $tiles
+     * @return BoardState
+     */
     public function add($tiles): BoardState
     {
         if (! is_array($tiles)) {
@@ -51,10 +66,26 @@ class BoardState implements Arrayable, Jsonable
             $currentState = $this->state[$tile->getRow()];
             $currentState[$tile->getRowPosition()] = $tile->getType();
 
+            $this->moves->push([
+                'x' => $tile->getRowPosition(),
+                'y' => $tile->getRow(),
+                'unit' => $tile->getType(),
+            ]);
+
             $this->state[$tile->getRow()] = $currentState;
         }
 
         return $this;
+    }
+
+    /**
+     * Returns the moves made in the board.
+     *
+     * @return Collection
+     */
+    public function getMoves(): Collection
+    {
+        return $this->moves;
     }
 
     /**
--- tests/BoardStateTest.php
+++ tests/BoardStateTest.php
@@ -23,6 +23,44 @@ class BoardStateTest extends TestCase
     }
 
     /** @test */
+    public function it_can_add_new_states()
+    {
+        $state = app(BoardState::class); // BoardState singleton
+        $state->add([
+            (new Tile)->withType(TileType::O)->withPosition(1),
+            (new Tile)->withType(TileType::X)->withPosition(2),
+            (new Tile)->withType(TileType::O)->withPosition(9)
+        ]);
+
+        $expectedStateArray = [
+            [TileType::O, TileType::X, ''],
+            ['', '', ''],
+            ['', '', TileType::O],
+        ];
+
+        $this->assertEquals($expectedStateArray, $state->toArray());
+    }
+
+    /** @test */
+    public function it_can_show_moves_history()
+    {
+        $state = app(BoardState::class); // BoardState singleton
+        $state->add([
+            (new Tile)->withType(TileType::O)->withPosition(1),
+            (new Tile)->withType(TileType::X)->withPosition(2),
+            (new Tile)->withType(TileType::O)->withPosition(9)
+        ]);
+
+        $expected = [
+            ['x' => 0, 'y' => 0, 'unit' => TileType::O],
+            ['x' => 1, 'y' => 0, 'unit' => TileType::X],
+            ['x' => 2, 'y' => 2, 'unit' => TileType::O],
+        ];
+
+        $this->assertEquals($expected, $state->getMoves()->toArray());
+    }
+
+    /** @test */
     // public function it_can_determine_if_move_is_valid()
     // {
     //     $this->expectException(InvalidBoardStateException::class);
